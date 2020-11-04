e9ce49471e95b49e546070d743b7a4edc9c509f8
吴少青 <wushaoqing@inke.cn>
2020-11-04 20:43:06 +0800

    pyramids first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿// app.js是小程序的脚本代码。我们可以在这个文件中监听并处理小程序的生命周期函数、声明全局变量。调用框架提供的丰富的 API，如本例的同步存储及同步读取本地数据。
+App({
+    onLaunch: function () {
+      console.log('App Launch')
+    },
+    onShow: function () {
+      console.log('App Show')
+    },
+    onHide: function () {
+      console.log('App Hide')
+    }
+  })
